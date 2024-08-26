8866db99be9637e97452bc0cf6c329c357cca02e
rajchandankumar <119934381+rajchandankumar@users.noreply.github.com>
2024-08-26 19:16:24 +0530

    basic 2

---
+++ 02_basic/01_function.js
@@ -0,0 +1,28 @@
+// syntax of function
+
+// function funName(){
+
+// }
+
+//  Arrow function
+
+// () => {
+
+// }
+
+//  first class function , which can be store in variable
+// const fun = () => {
+//     console.log('function body');
+    
+// }
+// console.log(fun());
+
+
+// iffi = imediately invocked function
+// (function(){})()
+
+
+// (function(){
+//     console.log("iffi function");
+    
+// })()
---
+++ 02_basic/02_Array.js
@@ -0,0 +1,29 @@
+const arr = new Array(1,2,3,4,5)
+// console.log(arr.length);
+// arr.push(6)
+// console.log(arr);
+
+// console.log(arr.pop());
+// console.log(arr);
+
+// console.log(
+//  arr.join(""));
+
+// const arr2 = arr.concat([10,11]);
+// console.log(arr2);
+
+
+// arr.map(function(ele,index, arr){
+//     const num = ele*2;
+//     console.log(num, index, arr);
+    
+// })
+
+// arr.forEach(function(ele){
+//     console.log(ele);
+    
+// })
+
+// const filterarr = arr.filter((elem) => elem%2 == 0)
+// console.log(filterarr);
+
